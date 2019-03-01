d2f020540170c6c4ba653d810fbe0c42311ddafe
almoskvin <sil.moskvin@gmail.com>
2019-03-01 17:32:31 +0300

    Initial commit

---
+++ src/main/java/com/github/almoskvin/urlshortener/model/UrlLinker.java
@@ -0,0 +1,58 @@
+package com.github.almoskvin.urlshortener.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+public class UrlLinker {
+
+    @Id
+    private String id;
+
+    private String shortUrl;
+
+    private String longUrl;
+
+    @CreatedDate
+    private Date createdDate;
+
+    public UrlLinker(String shortUrl, String longUrl) {
+        this.shortUrl = shortUrl;
+        this.longUrl = longUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
